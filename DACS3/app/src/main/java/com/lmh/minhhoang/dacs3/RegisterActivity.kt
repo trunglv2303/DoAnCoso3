68f435abf3cdad57a5d64b77377f6235634d0b82
Lê Minh Hoàng <117622199+hoan9coder@users.noreply.github.com>
2024-03-19 20:14:46 +0700

    19032024_UpdateRegisterConnectCSDL

--- DACS3/app/src/main/java/com/lmh/minhhoang/dacs3/RegisterActivity.kt
+++ DACS3/app/src/main/java/com/lmh/minhhoang/dacs3/RegisterActivity.kt
@@ -2,44 +2,51 @@ package com.lmh.minhhoang.dacs3
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.lmh.minhhoang.dacs3.databinding.ActivityRegisterBinding
+import com.lmh.minhhoang.dacs3.model.Users
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var fb: FirebaseAuth
-    private lateinit var binding: ActivityRegisterBinding;
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var user: Users
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_register);
-        fb= FirebaseAuth.getInstance()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+        fb = FirebaseAuth.getInstance()
+        user = Users()
+
         binding.buttonRegister.setOnClickListener {
-            register();
+            register()
         }
+
         binding.yesuser.setOnClickListener {
-            val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
-            startActivity(intent);
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
-    private fun register() {
-        val name = binding.name.text.toString()
-        val email = binding.username.text.toString()
-        val password = binding.password.text.toString()
 
-        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+    private fun register() {
+        user.name = binding.name.text.toString()
+        user.email = binding.username.text.toString()
+        user.password = binding.password.text.toString()
+
+        if (user.name.isEmpty() || user.email.isEmpty() || user.password.isEmpty()) {
             Toast.makeText(this@RegisterActivity, "Vui Lòng Nhập Đầy Đủ Thông Tin", Toast.LENGTH_LONG).show()
         } else {
-            fb.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            fb.createUserWithEmailAndPassword(user.email, user.password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Toast.makeText(applicationContext, "Đăng Kí Thành công", Toast.LENGTH_LONG).show()
-                    val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    Firebase.firestore.collection("Users").document(fb.currentUser!!.uid).set(user)
+                        .addOnSuccessListener {
+                            Toast.makeText(applicationContext, "Đăng Kí Thành công", Toast.LENGTH_LONG).show()
+                        }
                 } else {
                     val errorMessage = task.exception?.message ?: "Lỗi không xác định"
                     Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_LONG).show()
@@ -47,5 +54,4 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
     }
-
 }
--- DACS3/app/src/main/java/com/lmh/minhhoang/dacs3/model/Users.java
+++ DACS3/app/src/main/java/com/lmh/minhhoang/dacs3/model/Users.java
@@ -1,14 +1,44 @@
 package com.lmh.minhhoang.dacs3.model;
 
 public class Users {
+    private String thumb;
+    private String address;
+    private String numberPhone;
     private String email;
     private String password;
     private String name;
 
-//    public Users(String email, String password) {
-//        this.email = email;
-//        this.password = password;
-//    }
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getEmail() {
         return email;
